3ea2de971ebf505def166717f2e76b776cb035c5
Yavorskiy Maksim <maxfatal@mail.ru>
2023-01-25 15:24:09 +0300

    docs: send to autotest

--- src/index.js
+++ src/index.js
@@ -35,12 +35,36 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    'space': ' ',   
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let word = '';
+    let temp10Char = '';
+    for(let i =0; i<expr.length; i= i +10) {
+      let morseString = ''; 
+      temp10Char = expr.slice(i, i+10);
+        for(let k=0; k <temp10Char.length; k=k+2){
+          if (( temp10Char.slice(temp10Char.length -2 -k, temp10Char.length -k)) === '11') {
+              morseString =  '-' + morseString
+          }
+             else if(( temp10Char.slice(temp10Char.length -2 -k, temp10Char.length -k)) === '10') {
+                morseString = '.' + morseString 
+              }
+                 else if (( temp10Char.slice(temp10Char.length -2 -k, temp10Char.length -k)) === '**') {  
+                morseString = morseString + 'space'
+                k=k+8;
+                 }
+          }
+          word += MORSE_TABLE[morseString];   
+        }
+          return word;
+    }
+
 
 module.exports = {
     decode
 }
+
+
+
